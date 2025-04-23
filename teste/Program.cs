4e5bffdf158f4e94114636b917c63945f372ad37
J.U.!.C.£ <marquessilvaluana22@gmail.com>
2025-04-23 09:26:12 -0300

    foi

--- teste/Program.cs
+++ teste/Program.cs
@@ -1,3 +1,6 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digitr a idade");
+int idade = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Digite sua altura em Centímentros");
+double altura = double.Parse(Console.ReadLine());
